47519bbacbab5fe9245b8951f12554f1dbcb5656
CHAM CHANG CHUAN <chamchangchuan@gmail.com>
2022-10-12 17:48:17 +0800

    Page update
    UI update:
    1. Added several pages for personal self-introduction home page

--- angular-link-to-DB/link-db-project/src/app/app.module.ts
+++ angular-link-to-DB/link-db-project/src/app/app.module.ts
@@ -11,6 +11,12 @@ import { UserDeleteComponent } from './user/user-list/user-delete/user-delete.co
 import { UserAddComponent } from './user/user-add/user-add.component';
 import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
+import { IntroductionComponent } from './home/introduction/introduction.component';
+import { ExperienceComponent } from './home/experience/experience.component';
+import { ServicesOfferComponent } from './home/services-offer/services-offer.component';
+import { GalleryComponent } from './home/gallery/gallery.component';
+import { FeedbackComponent } from './home/feedback/feedback.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,13 @@ import { HomeComponent } from './home/home.component';
     UserDeleteComponent,
     UserAddComponent,
     UserComponent,
-    HomeComponent
+    HomeComponent,
+    IntroductionComponent,
+    ExperienceComponent,
+    ServicesOfferComponent,
+    GalleryComponent,
+    FeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-link-to-DB/link-db-project/src/app/home/services-offer/services-offer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServicesOfferComponent } from './services-offer.component';
+
+describe('ServicesOfferComponent', () => {
+  let component: ServicesOfferComponent;
+  let fixture: ComponentFixture<ServicesOfferComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServicesOfferComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ServicesOfferComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
